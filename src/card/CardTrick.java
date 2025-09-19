76a9da105448f6d0f9ae04e80006ee8727787e95
Abdul Rahman Pennino <72480260+nebulavoyager21@users.noreply.github.com>
2025-09-19 19:06:52 -0400

    Update CardTrick.java
    Added lucky card section via GitHub after accidentally pushing an earlier version and removing it in a previous commit.

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -70,6 +71,26 @@ public class CardTrick {
             System.out.println("Congratulations! Your card is in the magic hand!");
         } else {
             System.out.println("Sorry, your card is not in the magic hand.");
+        }
+        
+        // add one luckcard hard code 2,clubs
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Clubs");
+        
+        boolean luckyFound = false;
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = magicHand[i];
+            if (c.getValue() == luckyCard.getValue() && c.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+                luckyFound = true;
+                break;
+            }
+        }
+        
+        if (luckyFound) {
+            System.out.println("Lucky card is in the magic hand!");
+        } else {
+            System.out.println("Lucky card is not in the magic hand.");
         }
         
         input.close();      
